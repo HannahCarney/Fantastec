b1838c2d1627e70bfff246901505510f46f0c886
Hannah Carney <hannahcarney@Hannahs-MacBook-Pro.local>
2020-02-07 15:19:31 +0000

    eslint typescript prettier

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,6 @@
-module.exports = {
-  bracketSpacing: false,
-  jsxBracketSameLine: true,
-  singleQuote: true,
-  trailingComma: 'all',
+module.exports =  {
+    "singleQuote": true,
+    "trailingComma": "none",
+    "tabWidth": 4,
+    "semi": true
 };
